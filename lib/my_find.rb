58f4d064ebf9e1c8232625a3dae1340eaebdbd8c
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-12 17:35:02 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   counter=0
     while counter < collection.length
        if yield(collection[counter])==
-         
+         return collection[counter]
       
       counter+=1
     end
